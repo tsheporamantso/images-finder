a45b0cbb2ebc87d6f4b66c735dc289a2fa6a3d0f
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-14 10:05:23 +0200

    Add logic to toggle between dark and light theme

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -8,7 +8,10 @@ export const AppProvider = ({ children }) => {
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+
+    document.body.classList.toggle("dark-theme");
   };
+
   return (
     <AppContext.Provider value={{ toggleDarkTheme, isDarkTheme }}>
       {children}
