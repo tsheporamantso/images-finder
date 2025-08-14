5e1f44233eddae0d879a0e1cab03006e9c63e5fd
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-14 09:31:06 +0200

    recieve values from custom hook, import and render react icons conditionally

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { GoSun } from "react-icons/go";
+import { FaMoon } from "react-icons/fa";
 import { useGlobalContext } from "../context/context";
 
 const ThemeToggle = () => {
-  const { greetings } = useGlobalContext();
-  console.log(greetings);
-  return <div>ThemeToggle</div>;
+  const { toggleDarkTheme, isDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <GoSun className="toggle-icon" />
+        ) : (
+          <FaMoon className="toggle-icon" />
+        )}
+      </button>
+    </section>
+  );
 };
 
 export default ThemeToggle;
