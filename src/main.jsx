5cb5925e688c4c468d6c59e167f2d0d85e19045d
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-14 18:56:09 +0200

    Import reactquery dev tools

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { AppProvider } from "./context/context";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { StrictMode } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,7 @@ createRoot(document.getElementById("root")).render(
     <AppProvider>
       <QueryClientProvider client={queryClient}>
         <App />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </AppProvider>
   </StrictMode>
