dfe82ea6451ee0a7784dc0f86d42e781ddeac19f
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-14 18:22:43 +0200

    Import query client provider and query client from react query and wrap app with provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import "./index.css";
 import App from "./App.jsx";
 import { AppProvider } from "./context/context";
 import "react-toastify/dist/ReactToastify.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { StrictMode } from "react";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <AppProvider>
-    <App />
-  </AppProvider>
+  <StrictMode>
+    <AppProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </AppProvider>
+  </StrictMode>
 );
