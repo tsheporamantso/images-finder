04b55312ff33f33760547d19bd6ab4f224c83a3f
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-14 12:11:23 +0200

    Add form with input and button and also add inout control with guard claws

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!searchValue) {
+      toast.error("Cannot submit empty field!");
+      return;
+    }
+  };
+
+  return (
+    <section>
+      <h1 className="title">Images Finder</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          placeholder="Search Image"
+          className="form-input search-input"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
