dc1faf6fd74f633c7a624c462fa3ba660a4338ad
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-15 10:11:33 +0200

    Implement dark mode functionality with initial state management

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { AppContext } from "./AppContext";
+
+const getInitialDarkMode = () => {
+  const prefersDarkTheme = window.matchMedia(
+    "(prefers-color-scheme:dark)"
+  ).matches;
+  const storedDarkMode = localStorage.getItem("darkTheme") === "false";
+
+  return storedDarkMode || prefersDarkTheme;
+};
+
 export const AppProvider = ({ children }) => {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(getInitialDarkMode());
   const [searchTerm, setSearchTerm] = useState("flowers");
 
   const toggleDarkTheme = () => {
     const newDarkTheme = !isDarkTheme;
     setIsDarkTheme(newDarkTheme);
+    localStorage.setItem("darkTheme", newDarkTheme);
+  };
 
-    if (newDarkTheme) {
+  useEffect(() => {
+    if (isDarkTheme) {
       document.body.classList.add("dark-theme");
     } else {
       document.body.classList.remove("dark-theme");
     }
-  };
+  }, [isDarkTheme]);
 
   return (
     <AppContext.Provider
